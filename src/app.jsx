efc4476751ca8fb8794c541f89d8e7b95aab7b04
Jerry smith <jcsmith115@outlook.com>
2021-02-24 11:13:31 -0500

    Replaced sort drop down with sort buttons

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider, Connect } from 'react-redux'
+import { Provider } from 'react-redux'
+import { addExpense } from './redux/expenses/actions'
 import AppRouter from './router/AppRouter'
 import configStore from './redux/store/store'
-import { addExpense, editExpense, deleteExpense } from './redux/expenses/actions'
-import { setTextFilter, sortBy, setDateRange } from './redux/filters/actions'
-import filteredExpenses from './redux/selectors/filteredExpenses'
 import './styles/styles.sass'
 
 const store = configStore()
-
-store.subscribe(() => {
-  const state = store.getState()
-  const filtered = filteredExpenses( state.expenses, state.filters )
-  // console.log(filtered)
-})
-
 
 store.dispatch(addExpense({ description: 'water', amount: 30, createdAt: 100 }))
 store.dispatch(addExpense({ description: 'cake', amount: 520, createdAt: 8432100 }))
--- src/components/ExpenseListFilters.jsx
+++ src/components/ExpenseListFilters.jsx
@@ -1,20 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { setTextFilter, sortBy } from '../redux/filters/actions'
+import SortButton from './SortButton'
 
 const ExpenseListFilters = props => {
-
-  const sortOptions = [
-    { type: 'date', desc: true, text: 'Date', code: '8659' },
-    { type: 'date', desc: false, text: 'Date', code: '8657' },
-    { type: 'amount', desc: true, text: 'Amount', code: '8659' },
-    { type: 'amount', desc: false, text: 'Amount', code: '8657' }
-  ]
-
-  const setSortBy = ({ type, desc })=> {
-    props.dispatch(sortBy({ type, desc }))
-  }
-
   return (
     <div>
       <input type="text"
@@ -22,25 +11,11 @@ const ExpenseListFilters = props => {
         onChange={ e =>
           props.dispatch(setTextFilter( e.target.value ))}
         />
-
-        <select
-          onChange={ e => setSortBy( JSON.parse(e.target.value ))}>
-
-        { sortOptions.map( ( option, i ) => (
-
-          <option key={ i }
-            value={ JSON.stringify( option )}>
-            { `${option.text} ${String.fromCharCode(option.code)}`}
-          </option>
-
-        ))}
-        </select>
-
+        <SortButton buttonName="Date" sortBy={ props.filters.sortBy } />
+        <SortButton buttonName="Amount" sortBy={ props.filters.sortBy } />
     </div>
   )
 }
-
-
 
 const mapState = state => ({
   filters: state.filters
