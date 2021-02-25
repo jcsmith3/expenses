2357be5a20513d6a452e9ffb6cdb54f336c866a4
Jerry smith <jcsmith115@outlook.com>
2021-02-24 19:08:43 -0500

    Added simple express server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 const publicPath = path.join( __dirname,'..', 'public' )
-const PORT = node.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 app.use( express.static( publicPath ))
 
